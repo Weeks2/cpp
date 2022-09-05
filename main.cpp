1a309e48e3f6c1bdf4669af9d2b710ba6d82ecff
Weeks2 <106888385+Weeks2@users.noreply.github.com>
2022-09-05 17:17:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,23 @@ void GenerateReverseInputFiles();
 
 int A1000[1000], B4000[4000], C8000[8000], D10000[10000], E40000[40000], F80000[80000], G100000[100000], H400000[400000], I800000[800000], J1000000[1000000];
 
+void sortA() 
+{
+ int temp = 0;
+ for(int i = 0; i< 1000; i++) 
+ {
+   for(int j = i+1; j<10; j++)
+   {
+      if(A1000[j] < A1000[i]) 
+      {
+         temp = A1000[i];
+         A1000[i] = A1000[j];
+         A1000[j] = temp;
+      }
+    }    
+  }
+}
+
 int main()
 {
     GenerateRandomInputFiles();
