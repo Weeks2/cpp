f528c4dd17d2e13333a9e2f3a985c2bb04657090
Weeks2 <106888385+Weeks2@users.noreply.github.com>
2022-09-05 17:04:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,3 @@
-/** 
-Escriba tres funciones que generen cada una 10 archivos de conjunto de datos únicos de enteros separados por una coma ",". 
-Los tamaños de estos 10 conjuntos de datos son (n) – 1000, 4000, 8000, 10000, 40000, 80000, 100000, 400000, 800000, 1000000. 
-
-Función 1:  generar enteros aleatorios entre 0 y 1.000.000 como elementos de cada conjunto de datos.
-Función 2:  generar un archivo con una lista ordenada de enteros de menor a mayor. 
-Función 3:  generar un archivo con una lista ordenada inversamente con valores que van de mayor a menor. 
-
-Por ejemplo, 
-
-1000Sorted file tendrá de 0 a 999, 
-1000Reverse.txt tendrá de 999 a 0 y 
-1000Random.txt tendrá números aleatorios entre 0 y 1,000,000.
-
-**/
 #include<iostream>
 #include<stdlib.h>
 #include<time.h>
@@ -40,7 +25,7 @@ void GenerateRandomInputFiles()
         randonNumber = rand()%(1000000+1-first)+first;
         if(i < 1000) 
         {
-            A1000[i] = rnum;
+            A1000[i] = randonNumber;
         }
         else 
         { 
