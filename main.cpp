b5209b6ecbbb1df79577bbddaf80f847b45523a9
Weeks2 <106888385+Weeks2@users.noreply.github.com>
2022-09-05 17:26:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<stdlib.h>
 #include<time.h>
+#include <fstream>
 using namespace std;
 
 void GenerateRandomInputFiles();
@@ -9,12 +10,12 @@ void GenerateReverseInputFiles();
 
 int A1000[1000], B4000[4000], C8000[8000], D10000[10000], E40000[40000], F80000[80000], G100000[100000], H400000[400000], I800000[800000], J1000000[1000000];
 
-void sortA() 
+void sortA1000() 
 {
- int temp = 0;
- for(int i = 0; i< 1000; i++) 
+  int i, j,temp;
+ for( i = 0; i< 1000; i++) 
  {
-   for(int j = i+1; j<10; j++)
+   for( j = i+1; j<1000; j++)
    {
       if(A1000[j] < A1000[i]) 
       {
@@ -46,7 +47,13 @@ void GenerateRandomInputFiles()
         }
         else 
         { 
-            cout<<A1000[999];
+            sortA1000();
+            cout<<A1000[0]<<"--1"<<endl;
+            cout<<A1000[1]<<"--2"<<endl;
+            cout<<A1000[2]<<"--3"<<endl;
+            cout<<A1000[997]<<"--1"<<endl;
+            cout<<A1000[998]<<"--2"<<endl;
+            cout<<A1000[999]<<"--3"<<endl;
             break;
         }
     }
