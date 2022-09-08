8d48e2862233914e0cd0619e4765d981273bd3b9
Weeks2 <106888385+Weeks2@users.noreply.github.com>
2022-09-07 21:05:30 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,47 @@
 #include<iostream>
 #include<stdlib.h>
 #include<time.h>
+#include <vector>
+#include <string>
 #include <fstream>
+#include <sstream>
+#include <algorithm>
 using namespace std;
 
-void bubbleSort(int *arraySize, int len);
+int NUM_MAX = 1000000;
+int A1000[1000], B4000[4000], C8000[8000], D10000[10000],E40000[40000], F80000[80000], G100000[100000],H400000[400000], I800000[800000], J1000000[1000000];
+
+void reverse(int *arr, int lenght);
+int partition(int *ar, int l, int r);
+
+void quickSort(int *ar, int low, int high);
+void bubbleSort(int *ar, int length);
+void printArray(string flag,int *ar, int length);
+
 void GenerateRandomInputFiles();
 void GenerateSortedInputFiles();
-void GenerateReverseInputFiles(); 
+void GenerateReverseInputFiles();
 
-int A1000[1000], B4000[4000], C8000[8000], D10000[10000], E40000[40000], F80000[80000], G100000[100000], H400000[400000], I800000[800000], J1000000[1000000];
 
 int main()
 {
     GenerateRandomInputFiles();
-    //GenerateSortedInputFiles();
-    //GenerateReverseInputFiles(); 
-    cout<<endl;
+    //printArray("A1000:",A1000,1000);
+   // GenerateSortedInputFiles();
+    //GenerateReverseInputFiles();
     return 0;
 }
-
-void GenerateRandomInputFiles() 
+void GenerateRandomInputFiles()
 {
     int randonNumber, first = 0;
     srand(time(0));
-    for(int i = 0; i < 1000000; i++)
+    for(int i = 0; i < NUM_MAX; i++)
     {
-        randonNumber = rand()%(1000000+1-first)+first;
-        cout<<randonNumber<<endl;
+        randonNumber = rand()%(NUM_MAX+1-first)+first;
+        cout<<"GENERATING:"<<randonNumber<<endl;
         if(i < 1000) { A1000[i] = randonNumber;}
         if(i < 4000) { B4000[i] = randonNumber;}
+        /**
         if(i < 8000) { C8000[i] = randonNumber;}
         if(i < 10000) { D10000[i] = randonNumber;}
         if(i < 40000) { E40000[i] = randonNumber;}
@@ -37,27 +49,103 @@ void GenerateRandomInputFiles()
         if(i < 100000) { G100000[i] = randonNumber;}
         if(i < 400000) { H400000[i] = randonNumber;}
         if(i < 800000) { I800000[i] = randonNumber;}
-        if(i < 1000000) { J1000000[i] = randonNumber;}
-        if(i==1000000-1) {cout<<"STARTED:"<<J1000000[0]<<", FINISHED:"<<J1000000[i]<<endl;}       
-     }
+        if(i < NUM_MAX) { J1000000[i] = randonNumber;}
+        if(i==NUM_MAX-1) {cout<<"STARTED:"<<J1000000[0]<<", FINISHED:"<<J1000000[i]<<endl;}
+        **/
+    }
+}
+void GenerateSortedInputFiles()
+{
+     bubbleSort(A1000,1000);
+     bubbleSort(B4000,4000);
+     /**
+     bubbleSort(C8000,8000);
+     bubbleSort(D10000,10000);
+     bubbleSort(E40000,40000);
+     bubbleSort(F80000,80000);
+     bubbleSort(G100000,100000);
+     bubbleSort(H400000,400000);
+     bubbleSort(I800000,800000);
+     bubbleSort(J1000000,1000000);
+     **/
+}
+void GenerateReverseInputFiles()
+{
+    reverse(A1000,1000);
+    /**
+    reverse(B4000,4000);
+    reverse(C8000,8000);
+    reverse(D10000,10000);
+    reverse(E40000,40000);
+    reverse(F80000,80000);
+    reverse(G100000,100000);
+    reverse(H400000,400000);
+    reverse(I800000,800000);
+    reverse(J1000000,1000000);
+    **/
 }
 
-void GenerateSortedInputFiles() {}
-void GenerateReverseInputFiles() {}
 
-void bubbleSort(int arraySize[], int len)
+void bubbleSort(int *ar,int length)
 {
  int temp;
- for(int i = 0; i< len; i++)
+ for(int i = 0; i< length; i++)
  {
-   for(int j = i+1; j<len; j++)
+   for(int j = i+1; j<length; j++)
    {
-      if(arraySize[j] < arraySize[i])
+      if(ar[j] < ar[i])
       {
-         temp = arraySize[i];
-         arraySize[i] = arraySize[j];
-         arraySize[j] = temp;
+         temp = ar[i];
+         ar[i] = ar[j];
+         ar[j] = temp;
+         //cout<<"bubbleSort:"<<temp<<endl;
       }
     }
+
   }
+}
+
+void quickSort(int *ar, int low, int high)
+{
+    //quickSort(ar, 0, length - 1);
+    if (low < high)
+    {
+        int p = partition(ar, low, high);
+        quickSort(ar, 0, p - 1);
+        quickSort(ar, p + 1, high);
+        cout<<"QUICKSORT PARTITION:"<<p<<endl;
+    }
+}
+
+int partition(int *ar, int l, int r)
+{
+    int pivot = ar[r];
+    int i = l;
+    for (int j = l; j < r; j++)
+    {
+        if (ar[j] <= pivot)
+        {
+            int t = ar[j];
+            ar[j] = ar[i];
+            ar[i] = t;
+            i++;
+        }
+    }
+    int t = ar[i];
+    ar[i] = ar[r];
+    ar[r] = t;
+    return i;
+}
+
+void reverse(int *arr, int lenght)
+{
+    reverse(arr, arr + lenght);
+}
+
+void printArray(string flag, int *ar, int length)
+{
+    for(int i=0; i<length; i++)
+    {
+       cout<<flag<<ar[i]<<endl;
+    }
 }
