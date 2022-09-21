6875c3cda2aeb83f82842cbe1ac47bd11db988ed
Weeks2 <106888385+Weeks2@users.noreply.github.com>
2022-09-21 16:26:04 -0500

    Update main_sorting.cpp

--- main_sorting.cpp
+++ main_sorting.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+using namespace std;
+
+#include "bubbleSort.h"
+#include "insertionSort.h"
+#include "quickSort.h"
+
+int main()
+{
+    cout << "Sorting perform" << endl;
+
+    return 0;
+}
