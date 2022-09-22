a44d92d370549d2e81f9e08bbaab228092bb28ca
Weeks2 <106888385+Weeks2@users.noreply.github.com>
2022-09-22 16:11:16 -0500

    Update main_sorting.cpp

--- main_sorting.cpp
+++ main_sorting.cpp
@@ -21,26 +21,11 @@ void sortArray(string file, string method, int *ar, int length);
 int main()
 {
     cout<<"V1.0"<<"-----------------"<<endl;
-    generateArray("A1000",A1000,1000);
-    orderArrayBubbleSort("A1000",A1000,1000);
-    orderArrayQuickSort("A1000",A1000,1000);
-    orderArrayInsertionSort("A1000",A1000,1000);
-
-    generateArray("B4000",B4000,4000);
-    orderArrayBubbleSort("B4000",B4000,4000);
-    orderArrayInsertionSort("B4000",B4000,4000);
-    orderArrayQuickSort("B4000",B4000,4000);
-
-
-    generateArray("C8000",C8000,8000);
-    orderArrayBubbleSort("C8000",C8000,8000);
-    orderArrayInsertionSort("C8000",C8000,8000);
-    orderArrayQuickSort("C8000",C8000,8000);
-
-    generateArray("D10000",D10000,10000);
-    orderArrayBubbleSort("D10000",D10000,10000);
-    orderArrayQuickSort("D10000",D10000,10000);
-    orderArrayInsertionSort("D10000",D10000,10000);
+    
+    process("A1000",A1000,1000);
+    process("B4000",B4000,4000);
+    process("C8000",C8000,8000);
+    process("D10000",D10000,10000);
 
     displayDataSet("A1000_time_bubblesort");
     displayDataSet("A1000_time_quicksort");
@@ -50,50 +35,25 @@ int main()
     return 0;
 }
 
+void process(string file, int *ar, int length) 
+{
+    generateArray(file,ar,10000);
+    sortArray(file, "bubblesort",ar, length);
+    sortArray(file, "quicksort",ar, length);
+    sortArray(file, "insertionsort",ar, length);   
+}
+
 void sortArray(string file, string method, int *ar, int length)
 {
     auto start = chrono::steady_clock::now();
     readArray(file,ar,length);
     
-    if(method=="bubblesort").  { bubbleSort_opt(ar, length); }
-    if(method=="quicksort").   { quickSort(ar, 0, length - 1); }
+    if(method=="bubblesort")  { bubbleSort_opt(ar, length); }
+    if(method=="quicksort")   { quickSort(ar, 0, length - 1); }
     if(method=="insertionsort") { insertionSort(ar, length); }
     
     auto end = chrono::steady_clock::now();
     writeDataSetLine(file + "_time_" + method, chrono::duration_cast<chrono::milliseconds>(end - start).count(), length);
-}
-
-void orderArrayBubbleSort(string file, int *ar, int length)
-{
-    auto start = chrono::steady_clock::now();
-    readArray(file,ar,length);
-    bubbleSort_opt(ar, length);
-    auto end = chrono::steady_clock::now();
-    float elapsed = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-    writeDataSetLine(file + "_time_bubblesort", elapsed,length);
-    cout << "time elapsed:"<< elapsed<< " ms" << endl;
-}
-
-void orderArrayQuickSort(string file, int *ar, int length)
-{
-    auto start = chrono::steady_clock::now();
-    readArray(file,ar,length);
-    quickSort(ar, 0, length - 1);
-    auto end = chrono::steady_clock::now();
-    float elapsed = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-    writeDataSetLine(file + "_time_quicksort", elapsed,length);
-    cout << "time elapsed:"<< elapsed<< " ms" << endl;
-}
-
-void orderArrayInsertionSort(string file, int *ar, int length)
-{
-    auto start = chrono::steady_clock::now();
-    readArray(file,ar,length);
-    insertionSort(ar, length);
-    auto end = chrono::steady_clock::now();
-    float elapsed = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-    writeDataSetLine(file + "_time_insertionsort", elapsed,length);
-    cout << "time elapsed:"<< elapsed<< " ms" << endl;
 }
 
 void generateArray(string file, int *ar, int length)
