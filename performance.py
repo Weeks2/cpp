2830394efdab79cdd1c70782c2d71cf7ee77f03e
Weeks2 <106888385+Weeks2@users.noreply.github.com>
2022-09-23 12:29:49 -0500

    Create performance.py

---
+++ performance.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plot
+
+dataFrame = pd.DataFrame(pd.read_csv("dataset.csv", encoding='utf-8',names=["elapsed","size","sort"]));
+dataFrame = dataFrame.pivot(index="size",columns="sort",values="elapsed")
+dataFrame = dataFrame[["bubblesort","quicksort","insertionsort"]];
+dataFrame = dataFrame.fillna(0);
+print(dataFrame)
+
+dataFrame.plot(); 
+plot.show(block=True);
