1b38af910d86fda1dbb99c2517a202f177eea683
Weeks2 <106888385+Weeks2@users.noreply.github.com>
2022-09-23 13:54:10 -0500

    Update main_sorting.cpp

--- main_sorting.cpp
+++ main_sorting.cpp
@@ -13,9 +13,9 @@ using namespace std;
 int A1000[1000],B4000[4000],C8000[8000],D10000[10000];
 
 void generateArray(string file,int *ar, int length);
-void plotDataset();
 void sortArray(string file, string method, int *ar, int length);
 void process(string file, int *ar, int length);
+void plotDataset();
 
 int main()
 {
@@ -38,23 +38,6 @@ void generateArray(string file, int *ar, int length)
     printArray(file + " GENERATED -> ", ar, length);
 }
 
-void plotDataset()
-{
-    system("python3 dataset.py");
-    fstream newfile;
-    newfile.open("dataset.csv", ios::in);
-    string numberString;
-    int i = 0;
-    if (newfile.is_open())
-    {
-      while(getline(newfile, numberString,'\n'))
-      {
-         cout<<numberString<<endl;
-      }
-      newfile.close();
-    }
-}
-
 void sortArray(string file, string method, int *ar, int length)
 {
     auto start = chrono::steady_clock::now();
@@ -74,4 +57,21 @@ void process(string file, int *ar, int length)
     sortArray(file, "bubblesort",ar, length);
     sortArray(file, "quicksort",ar, length);
     sortArray(file, "insertionsort",ar, length);   
+}
+
+void plotDataset()
+{
+    system("python3 dataset.py");
+    fstream newfile;
+    newfile.open("dataset.csv", ios::in);
+    string numberString;
+    int i = 0;
+    if (newfile.is_open())
+    {
+      while(getline(newfile, numberString,'\n'))
+      {
+         cout<<numberString<<endl;
+      }
+      newfile.close();
+    }
 }
