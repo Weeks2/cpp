09ed6c50a607e695f9598f57336faafc3a45937b
Weeks2 <106888385+Weeks2@users.noreply.github.com>
2022-09-24 14:19:33 -0500

    Update dataset.py

--- performanceSort/dataset.py
+++ performanceSort/dataset.py
@@ -2,7 +2,5 @@ import pandas as pd
 import matplotlib.pyplot as plot
 
 dataFrame = pd.DataFrame(pd.read_csv("dataset.csv", encoding='utf-8',names=["elapsed","size","sort"]));
-dataFrame = dataFrame.pivot(index="size",columns="sort",values="elapsed").fillna(0)
-dataFrame.plot(); 
-
+dataFrame.pivot(index="size",columns="sort",values="elapsed").fillna(0).plot(); 
 plot.show();
