32b76d14ca3f399d742e7392b3637e8ab7b20d9a
Weeks2 <106888385+Weeks2@users.noreply.github.com>
2022-09-24 11:34:57 -0500

    Update dataset.py

--- performanceSort/dataset.py
+++ performanceSort/dataset.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plot
 
 dataFrame = pd.DataFrame(pd.read_csv("dataset.csv", encoding='utf-8',names=["elapsed","size","sort"]));
 dataFrame = dataFrame.pivot(index="size",columns="sort",values="elapsed").fillna(0)
-
 dataFrame.plot(); 
-plot.xlabel("Sorted numbers");
-plot.ylabel("Elapsed time miliseconds");
+
 plot.show();
