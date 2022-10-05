4e6e30f0c1becb8c8e9622b8fcdfbe49f6ec8dc9
Weeks2 <hola@weeks2.com>
2022-10-05 17:55:30 -0500

    Create utilsSort.h

---
+++ performanceSortHeap/utilsSort.h
@@ -0,0 +1,97 @@
+#ifndef UTILSSORT_H_INCLUDED
+#define UTILSSORT_H_INCLUDED
+
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <stdio.h>
+#include <cstring>
+
+int MAX_NUMBERS = 10000;
+void fillArray(int *ar, int length);
+void readArray(string filename, int *ar, int length);
+void printArray(string flag,int *ar, int length);
+
+void removeFile(string filename);
+void writeFile(string flag,int *ar, int length);
+
+
+void fillArray(int *ar, int length)
+{
+    cout<<"GERETING NUMBERS: "<<length<<endl;
+    int randonNumber, first = 0;
+    srand(time(0));
+    for (int i = 0; i < length; i++)
+    {
+        randonNumber = rand()%(MAX_NUMBERS+1-first)+first;
+        ar[i] = randonNumber;
+
+    }
+}
+
+void readArray(string filename, int *ar, int length)
+{
+    fstream newfile;
+    newfile.open(filename+".csv", ios::in);
+    string numberString;
+    int i = 0;
+    if (newfile.is_open())
+    {
+      while(getline(newfile, numberString,','))
+      {
+         ar[i] = stoi(numberString);
+      }
+      newfile.close();
+    }
+}
+
+void printArray(string flag, int *ar, int length)
+{
+    for(int i=0; i<length; i++)
+    {
+       cout<<flag<<ar[i]<<endl;
+    }
+}
+
+void removeFile(string filename)
+{
+    string filename_ = filename + ".csv";
+    int n = filename_.length();
+    char letters[n + 1];
+    strcpy(letters,filename_.c_str());
+
+    if(remove(letters) != 0 )
+    {
+     //cout<<"!DELETED "<<filename<<endl;
+     //perror( "Error deleting file" );
+    }
+    else
+    {
+     cout<<"DELETED "<<filename_<<endl;
+     //puts( "File successfully deleted" );
+    }
+}
+
+void writeFile(string flag,int *ar, int length)
+{
+    fstream fout;
+    fout.open(flag+".csv", ios::out | ios::app);
+
+    for (int i = 0; i < length; i++)
+    {
+        fout << ar[i] << (i < length-1 ? ",":"");
+    }
+}
+
+void writeDataSetLine(string flag, string method, float elapsed, int numbers)
+{
+    fstream fout;
+    fout.open(flag+".csv", ios::out | ios::app);
+    fout << elapsed <<","<<numbers<<","<<method<<"\n";
+}
+
+#endif // UTILSSORT_H_INCLUDED
